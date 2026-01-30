60086357c6575eaf0c708837b2b1bb4a3217048c
sahilgk-cms <sahil@catalysts.org>
2026-01-30 16:08:36 +0530

    made generic trainer

--- training/trainer.py
+++ training/trainer.py
@@ -6,16 +6,14 @@ class TimeSeriesTrainer:
         self.cv = cv
         self.metric_fn = metric_fn
 
-    def cross_validate(self, X, y, model_params, cat_features_indices):
+
+    def cross_validate(self, X, y, model_params, **model_kwargs):
         scores = []
 
         for fold, (train_idx, val_idx) in enumerate(self.cv.split(X)):
-            model = self.model_cls(
-                params=model_params,
-                cat_feature_indices=cat_features_indices
-            )
+            model = self.model_cls.from_params(model_params, **model_kwargs)
 
-            model.train(
+            model.fit(
                 X[train_idx],
                 y.iloc[train_idx],
             )
@@ -27,7 +25,7 @@ class TimeSeriesTrainer:
 
         return float(np.mean(scores))
 
-    def train_final_model(self, X, y, best_params, cat_features_indices):
-        model = self.model_cls(**best_params)
-        model.fit(X, y, cat_features=cat_features_indices)
+    def train_final_model(self, X, y, best_params, **model_kwargs):
+        model = self.model_cls.from_params(best_params, **model_kwargs)
+        model.fit(X, y)
         return model
