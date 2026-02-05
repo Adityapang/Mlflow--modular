6c164269bb1706d9f2cd4ec8266ed0ef8769f691
sahilgk-cms <sahil@catalysts.org>
2026-02-05 13:19:36 +0530

    updated paths

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ EXPERIMENT_NAME = "Tamil_Nadu_project"
 MLFLOW_URI = "http://127.0.0.1:5000/"
 
 ROLLING_WINDOW_MONTHS = None  # Keep last 24 months, set to None for all data
-N_OPTUNA_TRIALS = 2
+N_OPTUNA_TRIALS = 1
 N_CV_SPLITS = 5
 
 PREPROCESSOR_NAME = "tabular_v1"
@@ -23,12 +23,12 @@ TARGET_COL = "Case_Count_next_week"
 DISTRICT_COL = "dist_name"
 TEST_META_COLS = [DATE_COL, DISTRICT_COL, TARGET_COL]
 
-TRAIN_PATH = "train_dataset.csv"
-TEST_PATH = "test_dataset.csv"
-FEATURE_IMPORTANCE_PATH = "feature_importance.csv"
-PREDICTIONS_PATH = "predictions.csv"
-
-
+TRAIN_PATH = "train_dataset.parquet"
+TEST_PATH = "test_dataset.parquet"
+FEATURE_IMPORTANCE_PATH = "feature_importance.parquet"
+PREDICTIONS_PATH = "predictions.parquet"
+SHAP_VALUES_PATH = "shap_values.parquet"
+SHAP_SUMMARY_PATH =  "shap_summary.png"
 
 GPU_INFO = detect_gpu()
 USE_GPU = GPU_INFO["available"]
