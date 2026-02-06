4daad1a6469bbed342f023cdfa77556ae19a76ac
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2026-02-06 17:36:08 +0530

    updated to include metric spec

--- training/trainer.py
+++ training/trainer.py
@@ -1,31 +1,28 @@
 import numpy as np
 
 class TimeSeriesTrainer:
-    def __init__(self, model_cls, cv, metric_fn):
+    def __init__(self, model_cls, cv, metric):
+        """
+        metric: MetricSpec (name, fn, direction)
+        """
         self.model_cls = model_cls
         self.cv = cv
-        self.metric_fn = metric_fn
+        self.metric = metric
 
-
-    def cross_validate(self, X, y, model_params, **model_kwargs):
+    def evaluate_params(self, X, y, params, **model_kwargs) -> float:
         scores = []
 
-        for fold, (train_idx, val_idx) in enumerate(self.cv.split(X)):
-            model = self.model_cls.from_params(model_params, **model_kwargs)
-
-            model.fit(
-                X[train_idx],
-                y.iloc[train_idx],
-            )
+        for train_idx, val_idx in self.cv.split(X):
+            model = self.model_cls.from_params(params, **model_kwargs)
+            model.fit(X[train_idx], y.iloc[train_idx])
 
             preds = model.predict(X[val_idx])
-            score = self.metric_fn(y.iloc[val_idx], preds)
-
+            score = self.metric.fn(y.iloc[val_idx], preds)
             scores.append(score)
 
         return float(np.mean(scores))
 
-    def train_final_model(self, X, y, best_params, **model_kwargs):
+    def train_final(self, X, y, best_params, **model_kwargs):
         model = self.model_cls.from_params(best_params, **model_kwargs)
         model.fit(X, y)
         return model
