e5a91f21ba49803bcbf2616bd01d96592ceb0d7b
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2026-02-06 17:46:50 +0530

    added metric

--- data/prediction_builder.py
+++ data/prediction_builder.py
@@ -4,14 +4,14 @@ from config import DATE_COL, TARGET_COL, DISTRICT_COL
 
 def build_prediction_data(predictions: pd.Series,
                           X_test_meta: pd.DataFrame,
-                          best_cv_rmse: float,
-                          test_rmse: float) -> pd.DataFrame:
+                          best_cv_score: float,
+                          test_metrics: dict) -> pd.DataFrame:
     prediction_df = pd.DataFrame({
         DATE_COL: X_test_meta[DATE_COL],
         'predicted_case_count': predictions,
         'prediction_date': datetime.now().strftime('%Y-%m-%d'),
-        'best_cv_rmse': best_cv_rmse,
-        "test_rmse": test_rmse,
+        'best_cv_score': best_cv_score,
+        "test_metrics": test_metrics,
         DISTRICT_COL: X_test_meta[DISTRICT_COL],
         TARGET_COL: X_test_meta[TARGET_COL]
     })
